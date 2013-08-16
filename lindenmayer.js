4cf7cc06d76c33e356096f889bbc6b7afb5c12dc
Albert Veli <albert.veli@gmail.com>
2013-08-16 22:32:28 +0200

    First pages commit

---
+++ lindenmayer.js
@@ -0,0 +1,469 @@
+function debug_out(s, br) {
+    var x=document.getElementById("debug_area");
+    x.innerHTML += s;
+    if (br) {
+	x.innerHTML += '<br />';
+    }
+};
+
+function clear_debug() {
+    var x=document.getElementById("debug_area");
+    x.innerHTML = '';
+};
+
+// L-systems
+var lsystems = [];
+var current_lsystem;
+var lsys_name;
+var axiom;
+var rules = {};
+var pattern = '';
+var level = 3;
+var max_level = 9;
+
+var angle;
+var turn_angle;
+var linelen;
+var lendiv;
+var turtle_x;
+var turtle_y;
+
+var coords = [];
+var colours = [];
+var current_colour = 15;
+var multicolour = true;
+
+var palette = [
+    // Bright 0
+    0.0, 0.0, 0.0, 1.0, // 0 Black
+    0.0, 0.0, 0.5, 1.0, // 1 Blue
+    0.5, 0.0, 0.0, 1.0, // 2 Red
+    0.5, 0.0, 0.5, 1.0, // 3 Magenta
+    0.0, 0.5, 0.0, 1.0, // 4 Green
+    0.0, 0.5, 0.5, 1.0, // 5 Cyan
+    0.5, 0.5, 0.0, 1.0, // 6 Yellow
+    0.5, 0.5, 0.5, 1.0, // 7 White
+    // Bright 1
+    0.0, 0.0, 0.0, 1.0, // 8 Black
+    0.0, 0.0, 1.0, 1.0, // 9 Blue
+    1.0, 0.0, 0.0, 1.0, // 10 Red
+    1.0, 0.0, 1.0, 1.0, // 11 Magenta
+    0.0, 1.0, 0.0, 1.0, // 12 Green
+    0.0, 1.0, 1.0, 1.0, // 13 Cyan
+    1.0, 1.0, 0.0, 1.0, // 14 Yellow
+    1.0, 1.0, 1.0, 1.0  // 15 White
+];
+
+function push_colour() {
+    var i = current_colour * 4;
+    colours.push(palette[i + 0]);
+    colours.push(palette[i + 1]);
+    colours.push(palette[i + 2]);
+    colours.push(palette[i + 3]);
+}
+
+// clear() method for Array class
+Array.prototype.clear = function() {
+  while (this.length > 0) {
+    this.pop();
+  }
+};
+
+function render_arrays() {
+    coords.clear();
+    colours.clear();
+    var first = true;
+    var c;
+
+    for(i = 0; i < pattern.length; i++) {
+
+	switch (pattern[i]) {
+
+	case 'F': // Draw line Forward
+	    if (first) {
+		// First F in this line strip
+		coords.push(turtle_x);
+		coords.push(turtle_y);
+		push_colour();
+		first = false;
+	    }
+	    turtle_x += linelen * Math.cos(angle);
+	    turtle_y += linelen * Math.sin(angle);
+	    coords.push(turtle_x);
+	    coords.push(turtle_y);
+	    push_colour();
+	    break;
+
+	case 'f': // Move forward without drawing
+	    if (!first) {
+		// TODO: End this line strip
+	    }
+	    first = true;
+	    turtle_x += linelen * Math.cos(angle);
+	    turtle_y += linelen * Math.sin(angle);
+
+	case '+':
+	    angle += turn_angle;
+	    break;
+
+	case '-':
+	    angle -= turn_angle;
+	    break;
+
+	case 'C':
+	    i++;
+	    c = pattern[i];
+	    var num = '';
+	    while (c >= '0' && c <= '9' && i < pattern.length) {
+		num += c;
+		i++;
+		c = pattern[i];
+	    }
+	    current_colour = parseInt(num);
+	    i--; // increased by for loop
+	    break;
+	}
+    }
+};
+
+function angles_to_radians() {
+    angle *= 0.01745329251994;
+    turn_angle *= 0.01745329251994;
+}
+
+function lsys_levy() {
+    lsys_name = '<a href="http://en.wikipedia.org/wiki/L%C3%A9vy_C_curve" target="_blank">Levy C curve</a>';
+    if (multicolour) {
+	axiom = '+C4F--C12F+';
+	linelen = 2.0;
+    } else {
+	axiom = 'F';
+	linelen = 3.0;
+    }
+    rules["F"] = '+F--F+';
+    rules["+"] = '+';
+    rules["-"] = '-';
+    pattern = axiom;
+    turn_angle = 45;
+    lendiv = Math.sqrt(2);
+    current_colour = 15;
+    max_level = 19;
+    // Initial turtle position and angle
+    turtle_x = -1.5;
+    turtle_y = -1.0;
+    angle = 0;
+    angles_to_radians();
+};
+
+function lsys_terdragon() {
+    lsys_name = '<a href="http://en.wikipedia.org/wiki/Dragon_curve#Terdragon" target="_blank">Terdragon</a>';
+    if (multicolour) {
+	axiom = 'C2F+C14F-C4F';
+	angle = -45 - (30 * level);
+	linelen = 1.7;
+    } else {
+	axiom = 'F';
+	angle = -20 - (30 * level);
+	linelen = 3.0;
+    }
+    rules["F"] = 'F+F-F';
+    rules["+"] = '+';
+    rules["-"] = '-';
+    pattern = axiom;
+    turn_angle = 120;
+    lendiv = Math.sqrt(3);
+    current_colour = 15;
+    max_level = 12;
+    turtle_x = -1.5;
+    turtle_y = 0.5;
+    angles_to_radians();
+};
+
+function lsys_twindragon() {
+    lsys_name = '<a href="http://en.wikipedia.org/wiki/Dragon_curve#Twindragon" target="_blank">Twindragon</a> (Davis-Knuth)';
+    if (multicolour) {
+	axiom = 'C4Fx+C14Fx+';
+    } else {
+	axiom = 'Fx+Fx-';
+    }
+    rules["F"] = '';
+    rules["x"] = 'x+yF';
+    rules["y"] = 'Fx-y';
+    rules["+"] = '+';
+    rules["-"] = '-';
+    pattern = axiom;
+    turn_angle = 90;
+    angle = -20 - (45 * level);
+    linelen = 3.0;
+    lendiv = Math.sqrt(2);
+    current_colour = 15;
+    max_level = 19;
+    turtle_x = -0.961;
+    turtle_y = -0.448;
+    angles_to_radians();
+};
+
+function lsys_dragon() {
+    lsys_name = '<a href="http://en.wikipedia.org/wiki/Dragon_curve" target="_blank">Dragon</a> (Harter-Heighway)';
+    if (multicolour) {
+	axiom = 'C4Fx-C14Fy';
+	rules["x"] = 'Fx+Fy';
+	rules["y"] = 'Fx-Fy';
+	angle = -110 - (45 * level);
+	linelen = 1.5;
+	turtle_x = 1.13;
+	turtle_y = 0.77;
+    } else {
+	axiom = 'Fx';
+	rules["x"] = 'x+yF+';
+	rules["y"] = '-Fx-y';
+	angle = -20 - (45 * level);
+	linelen = 3.0;
+	turtle_x = -0.8;
+	turtle_y = -0.125;
+    }
+    rules["F"] = '';
+    rules["+"] = '+';
+    rules["-"] = '-';
+    pattern = axiom;
+    turn_angle = 90;
+
+    lendiv = Math.sqrt(2);
+    current_colour = 15;
+    max_level = 20;
+    angles_to_radians();
+};
+
+function lsys_snowflake() {
+    lsys_name = '<a href="http://en.wikipedia.org/wiki/Koch_snowflake" target="_blank">Koch snowflake</a>';
+    if (multicolour) {
+	axiom = 'C2F--C14F--C4F';
+    } else {
+	axiom = 'F--F--F';
+    }
+    rules["F"] = 'F+F--F+F';
+    rules["+"] = '+';
+    rules["-"] = '-';
+    pattern = axiom;
+    turn_angle = 60;
+    linelen = 3.0;
+    lendiv = 3;
+    current_colour = 15;
+    max_level = 9;
+    turtle_x = -1.5;
+    turtle_y = 0.85;
+    angle = 0;
+    angles_to_radians();
+};
+
+function lsys_flowsnake() {
+    lsys_name = '<a href="http://en.wikipedia.org/wiki/Gosper_curve" target="_blank">Flowsnake</a>';
+    if (multicolour) {
+	axiom = 'C2Fl-C6Fr--C3Fr+C4Fl++C10FlC14Fl+C12Fr-';
+	linelen = 1.2;
+	angle = 15;
+    } else {
+	axiom = 'Fl';
+	linelen = 3.0;
+	angle = 0;
+    }
+    rules["F"] = '';
+    rules["l"] = 'Fl-Fr--Fr+Fl++FlFl+Fr-';
+    rules["r"] = '+Fl-FrFr--Fr-Fl++Fl+Fr';
+    rules["+"] = '+';
+    rules["-"] = '-';
+    pattern = axiom;
+    turn_angle = 60;
+    lendiv = 2.65;
+    current_colour = 15;
+    max_level = 6;
+    turtle_x = -1.5;
+    turtle_y = 0.85;
+    angle += 19.3 * level;
+    angles_to_radians();
+};
+
+function lsys_sierpinski() {
+    lsys_name = '<a href="http://en.wikipedia.org/wiki/Sierpinski_triangle" target="_blank">Sierpinski triangle</a>';
+    if (multicolour) {
+	axiom = '+C2Fb-C14Fa-C4Fb+';
+	linelen = 1.5;
+    } else {
+	axiom = 'Fa';
+	linelen = 3;
+    }
+    rules["F"] = '';
+    rules["a"] = '+Fb-Fa-Fb+';
+    rules["b"] = '-Fa+Fb+Fa-';
+    rules["+"] = '+';
+    rules["-"] = '-';
+    pattern = axiom;
+    turn_angle = 60;
+    lendiv = 2.0;
+    current_colour = 15;
+    max_level = 12;
+    turtle_x = -1.5;
+    turtle_y = -1.33;
+    angle = 0;
+    angles_to_radians();
+};
+
+function lsys_church() {
+    lsys_name = '<a href="http://www.nahee.com/spanky/www/fractint/lsys/spacefilling.html" target="_blank">Dekking\'s Church</a>';
+    if (multicolour) {
+	axiom = 'C1wC7xC15yC7z';
+    } else {
+	axiom = 'wxyz';
+    }
+    linelen = 2;
+    rules["F"] = '';
+    rules["w"] = 'Fw+F-zFw-F+x';
+    rules["x"] = 'Fw+F-z';
+    rules["y"] = '--F++y+F-z';
+    rules["z"] = '--F++y-F+x--F++y-F+x';
+    rules["+"] = '+';
+    rules["-"] = '-';
+    pattern = axiom;
+    turn_angle = 90;
+    lendiv = 1.732;
+    current_colour = 15;
+    max_level = 11;
+    turtle_x = -1.732;
+    turtle_y = -1.0;
+    angle = 0;
+    angles_to_radians();
+};
+
+function clear_lsys() {
+    pattern = '';
+    axiom = '';
+    rules = {};
+};
+
+function expand_lsys() {
+    var i;
+    var new_pattern = '';
+
+    for(i = 0; i < pattern.length; i++) {
+	var c = pattern[i];
+
+	// Pass through any C followed by integer
+	if (c == 'C') {
+	    new_pattern += c;
+	    i++;
+	    c = pattern[i];
+	    while (c >= '0' && c <= '9' && i < pattern.length) {
+		new_pattern += c;
+		i++;
+		c = pattern[i];
+	    }
+	    if (i >= pattern.length) {
+		break; // finished
+	    }
+	}
+	// Normal rule
+	new_pattern += rules[c];
+    }
+    pattern = new_pattern;
+    linelen /= lendiv;
+};
+
+function out_keys() {
+    debug_out('<br />' + 'KEYS');
+    debug_out('<pre>' + 
+	      ' o,p        ; Cycle through L-systems' + '\n' +
+	      ' m          ; Toggle multi/mono colour' + '\n' +
+	      ' +,c        ; Increase level' + '\n' +
+	      ' -,x        ; Decrease level</pre>');
+    debug_out('CAMERA');
+    debug_out('<pre>' + 
+	      ' Up,Down    ; Zoom in/out (z-axis)' + '\n' +
+	      ' Left,Right ; Move left/right (x-axis)' + '\n' +
+	      ' f,v        ; Move up/down (y-axis)' + '\n' +
+	      ' Space      ; Reset camera</pre>');
+};
+
+var rendered;
+
+// Called every time lsystem or level is changed
+function init_lsystem() {
+    clear_debug();
+    lsystems[current_lsystem]();
+    // max_level might have changed
+    if (level > max_level) {
+	level = max_level;
+    }
+    debug_out('Calculating coordinates for ' + lsys_name + ' level: ' + level, true);
+    // Wait 10ms so message will be output before crunching begins
+    rendered = false;
+    setTimeout(init_lsystem2, 10);
+};
+
+// Continue here after small delay
+function init_lsystem2() {
+    var i;
+
+    for (i = 0; i < level; i++) {
+	expand_lsys();
+    }
+    render_arrays();
+    clear_debug();
+    debug_out('L-system: ' + lsys_name, true);
+    debug_out('Level: ' + level, true);
+    debug_out('Number of vertices: ' + coords.length / 2, true);
+    out_keys();
+    init_buffers();
+    rendered = true;
+};
+
+// Called only once, at startup
+function init_lindenmayer() {
+    // L-systems will appear in this order
+    lsystems.push(lsys_dragon);
+    lsystems.push(lsys_twindragon);
+    lsystems.push(lsys_sierpinski);
+    lsystems.push(lsys_terdragon);
+    lsystems.push(lsys_church);
+    lsystems.push(lsys_levy);
+    lsystems.push(lsys_snowflake);
+    lsystems.push(lsys_flowsnake);
+    current_lsystem = 0;
+    init_lsystem();
+};
+
+// Mostly OpenGL stuff below
+
+var linebuf;
+var colbuf;
+
+function init_buffers() {
+    linebuf = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, linebuf);
+    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(coords), gl.STATIC_DRAW);
+    linebuf.sz = 2;
+    linebuf.num_items = coords.length / 2;
+    colbuf = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, colbuf);
+    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(colours), gl.STATIC_DRAW);
+    colbuf.sz = 4;
+}
+
+function draw_scene() {
+    gl.viewport(0, 0, gl.viewportWidth, gl.viewportHeight);
+    gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+
+    mat4.identity(mv_matrix);
+
+    // Move camera
+    mat4.translate(mv_matrix, [xpos, ypos, zoom]);
+
+    // Draw line segments (if render is finised)
+    if (rendered) {
+	gl.bindBuffer(gl.ARRAY_BUFFER, linebuf);
+	gl.vertexAttribPointer(shader_program.vertex_pos, linebuf.sz, gl.FLOAT, false, 0, 0);
+	gl.bindBuffer(gl.ARRAY_BUFFER, colbuf);
+	gl.vertexAttribPointer(shader_program.vertex_col, colbuf.sz, gl.FLOAT, false, 0, 0);
+	set_mat_uniforms();
+	gl.drawArrays(gl.LINE_STRIP, 0, linebuf.num_items);
+    }
+}
---
+++ webglstuff.js
@@ -0,0 +1,314 @@
+// GL stuff, partly from lesson 2/6 at http://learningwebgl.com/
+var gl;
+var canvas;
+
+function init_gl() {
+    try {
+	gl = WebGLUtils.setupWebGL(canvas);
+	gl.viewportWidth = canvas.width;
+	gl.viewportHeight = canvas.height;
+    } catch (e) {
+    }
+    if (!gl) {
+	alert('Could not initialise WebGL');
+    }
+}
+
+function get_shader(gl, id) {
+    var shaderScript = document.getElementById(id);
+    if (!shaderScript) {
+	return null;
+    }
+
+    var str = '';
+    var k = shaderScript.firstChild;
+    while (k) {
+	if (k.nodeType == 3) {
+	    str += k.textContent;
+	}
+	k = k.nextSibling;
+    }
+
+    var shader;
+    if (shaderScript.type == 'x-shader/x-fragment') {
+	shader = gl.createShader(gl.FRAGMENT_SHADER);
+    } else if (shaderScript.type == 'x-shader/x-vertex') {
+	shader = gl.createShader(gl.VERTEX_SHADER);
+    } else {
+	return null;
+    }
+
+    gl.shaderSource(shader, str);
+    gl.compileShader(shader);
+
+    if (!gl.getShaderParameter(shader, gl.COMPILE_STATUS)) {
+	alert(gl.get_shaderInfoLog(shader));
+	return null;
+    }
+
+    return shader;
+}
+
+var shader_program;
+
+function init_shaders() {
+    var frag_sh = get_shader(gl, 'shader-fs');
+    var vert_sh = get_shader(gl, 'shader-vs');
+
+    shader_program = gl.createProgram();
+    gl.attachShader(shader_program, vert_sh);
+    gl.attachShader(shader_program, frag_sh);
+    gl.linkProgram(shader_program);
+
+    if (!gl.getProgramParameter(shader_program, gl.LINK_STATUS)) {
+	alert('Could not initialise shaders');
+    }
+
+    gl.useProgram(shader_program);
+
+    shader_program.vertex_pos = gl.getAttribLocation(shader_program, 'vx_pos');
+    gl.enableVertexAttribArray(shader_program.vertex_pos);
+
+    shader_program.vertex_col = gl.getAttribLocation(shader_program, 'vx_col');
+    gl.enableVertexAttribArray(shader_program.vertex_col);
+
+    shader_program.p_matrix = gl.getUniformLocation(shader_program, 'u_pmx');
+    shader_program.mv_matrix = gl.getUniformLocation(shader_program, 'u_mvmx');
+}
+
+var mv_matrix = mat4.create();
+var matrix_stack = [];
+var p_matrix = mat4.create();
+
+function push_matrix() {
+    var copy = mat4.create();
+    mat4.set(mv_matrix, copy);
+    matrix_stack.push(copy);
+}
+
+function pop_matrix() {
+    if (mv_matrix_stack.length == 0) {
+	throw 'Invalid popMatrix!';
+    }
+    mv_matrix = matrix_stack.pop();
+}
+
+function set_mat_uniforms() {
+    gl.uniformMatrix4fv(shader_program.p_matrix, false, p_matrix);
+    gl.uniformMatrix4fv(shader_program.mv_matrix, false, mv_matrix);
+}
+
+// Camera key controls
+var pressed_keys = {};
+var zoom = -7.0;
+var xpos = 0.0;
+var ypos = 0.0;
+var dz = 0.0;
+var dx = 0.0;
+var dy = 0.0;
+
+function handle_keys(elapsed) {
+    var maxx = 50.0;
+    var xchange = 0.0001 * elapsed;
+    if (pressed_keys[37]) {
+	// Left
+    } else  if (pressed_keys[39]) {
+	// Right
+	xchange *= -1;
+    } else {
+	xchange = 0;
+    }
+    dx += xchange * zoom;
+    dx /= 1.1;
+    xpos += dx;
+    if (xpos < -maxx) {
+    	xpos = -maxx;
+    	dx = 0;
+    } else if (xpos > maxx) {
+    	xpos = maxx;
+    	dx = 0;
+    }
+
+    var maxy = 50.0;
+    var ychange = 0.0001 * elapsed;
+    if (pressed_keys[70] || pressed_keys[85]) {
+	// Down
+	ychange *= -1;
+    } else  if (pressed_keys[75] || pressed_keys[86]) {
+	// Up
+    } else {
+	ychange = 0;
+    }
+    dy += ychange * zoom;
+    dy /= 1.1;
+    ypos += dy;
+    if (ypos < -maxy) {
+    	ypos = -maxy;
+    	dy = 0;
+    } else if (ypos > maxy) {
+    	ypos = maxy;
+    	dy = 0;
+    }
+
+    var zchange = 0.0002 * elapsed;
+    if (pressed_keys[38]) {
+	// Up
+	zchange *= -1;
+    } else  if (pressed_keys[40]) {
+	// Down
+    } else {
+	zchange = 0;
+    }
+    dz += zchange * zoom;
+    dz /= 1.1;
+    zoom += dz;
+    if (zoom > -0.01) {
+	zoom = -0.01;
+	dz = 0;
+    } else if (zoom < -99.9) {
+	zoom = -99.9;
+	dz = 0;
+    }
+}
+
+var last_time = 0;
+function animate() {
+    var now = new Date().getTime();
+    if (last_time != 0) {
+	var elapsed = now - last_time;
+	handle_keys(elapsed);
+    }
+    last_time = now;
+}
+
+function key_down(event) {
+    if (!pressed_keys[event.keyCode]) {
+	switch(event.keyCode) {
+	case 32:
+	    // Space, reset zoom
+	    dz = 0;
+	    dx = 0;
+	    zoom = -7;
+	    xpos = 0;
+	    ypos = 0;
+	    break;
+
+	case 38:
+	    // Up
+	    dz = zoom * -0.002;
+	    break;
+
+	case 40:
+	    // Down
+	    dz = zoom * 0.002;
+	    break;
+
+	case 37:
+	    // Left
+	    dx = zoom * -0.002;
+	    break;
+
+	case 39:
+	    // Right
+	    dx = zoom * 0.002;
+	    break;
+
+	case 70:
+	case 85:
+	    // - Move down
+	    dy = zoom * -0.002;
+	    break;
+
+	case 75:
+	case 86:
+	    // - Move up
+	    dy = zoom * 0.002;
+	    break;
+
+	case 107:
+	case 67:
+	case 74:
+	    // + or c
+	    if (level < max_level) {
+		// recalculate coords
+		level++;
+		init_lsystem();
+	    }
+	    break;
+
+	case 109:
+	case 81:
+	case 88:
+	    // - or x
+	    if (level > 0) {
+		// recalculate coords
+		level--;
+		init_lsystem();
+	    }
+	    break;
+
+	case 79:
+	case 82:
+	    // o
+	    if (current_lsystem > 0) {
+		current_lsystem--;
+		init_lsystem();
+	    }
+	    break;
+
+	case 80:
+	case 76:
+	    // p
+	    if (current_lsystem < lsystems.length - 1) {
+		current_lsystem++;
+		init_lsystem();
+	    }
+	    break;
+
+	case 68:
+	    // d
+	    clear_debug();
+	    debug_out(pattern, false);
+	    break;
+
+	case 77:
+	    // m, toggle multicolour
+	    if (multicolour) {
+		multicolour = false;
+	    } else {
+		multicolour = true;
+	    }
+	    init_lsystem();
+	    break;
+
+	}
+	pressed_keys[event.keyCode] = true;
+    }
+}
+
+function key_up(event) {
+    pressed_keys[event.keyCode] = false;
+}
+
+function render() {
+    requestAnimFrame(render, canvas);
+    draw_scene();
+    animate();
+}
+
+function start_webgl() {
+    canvas = document.getElementById('main_canvas');
+    init_gl();
+    init_shaders();
+    init_lindenmayer();
+
+    gl.clearColor(0.0, 0.0, 0.0, 1.0);
+    gl.enable(gl.DEPTH_TEST);
+
+    mat4.perspective(45, gl.viewportWidth / gl.viewportHeight, 0.01, 100.0, p_matrix);
+
+    document.onkeydown = key_down;
+    document.onkeyup = key_up;
+
+    render();
+}
