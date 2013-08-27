1c8cd2b85dbb56c179a7d8422ba3cd2964cbb68b
Albert Veli <albert.veli@gmail.com>
2013-08-27 16:26:13 +0200

    Skeleton for model part of MVC

---
+++ ios/LSystems/LSystems/Model/LindenmayerBrain.h
@@ -0,0 +1,13 @@
+//
+//  LindenmayerBrain.h
+//  LSystems
+//
+//  Created by Albert Veli at Prickle-Prickle
+//  the 20th day of Bureaucracy in the YOLD 3179
+//  (k) ALL RIGHTS REVERSED - Reprint what you like
+
+#import <Foundation/Foundation.h>
+
+@interface LindenmayerBrain : NSObject
+
+@end
---
+++ ios/LSystems/LSystems/Model/LindenmayerBrain.m
@@ -0,0 +1,13 @@
+//
+//  LindenmayerBrain.m
+//  LSystems
+//
+//  Created by Albert Veli at Prickle-Prickle
+//  the 20th day of Bureaucracy in the YOLD 3179
+//  (k) ALL RIGHTS REVERSED - Reprint what you like
+
+#import "LindenmayerBrain.h"
+
+@implementation LindenmayerBrain
+
+@end
